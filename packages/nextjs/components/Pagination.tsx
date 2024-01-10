550a7e077461b14658a5517bb755a4951c7ff29c
Dream4ever <cnhwvista@gmail.com>
2024-01-10 21:00:53 +0800

    feat: 分页组件样式适配深色模式

--- packages/nextjs/components/Pagination.tsx
+++ packages/nextjs/components/Pagination.tsx
@@ -6,7 +6,7 @@ type PaginationProps = {
 export const Pagination = () => {
   const Element = ({ text, active }: PaginationProps) => (
     <span
-      className={`flex items-center justify-center w-8 h-8 border border-[#EEEEEE] rounded bg-[#F5F5F5] ${
+      className={`flex items-center justify-center w-8 h-8 bg-[#F5F5F5] border border-[#EEEEEE] rounded dark:bg-[#171717] dark:border-[#1E1E1E] ${
         active && "bg-gradient-to-r from-gradFrom to-gradTo text-white"
       }`}
     >
@@ -15,7 +15,7 @@ export const Pagination = () => {
   );
 
   return (
-    <div className="flex items-center justify-between space-x-4">
+    <div className="flex items-center justify-between space-x-4 dark:text-[#404B52]">
       <Element text={"<"} />
       <Element text={"1"} active={true} />
       <Element text={"2"} />
