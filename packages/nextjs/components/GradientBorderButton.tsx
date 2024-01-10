30e73638e52da795a9331d3f457c0890159b3c33
Dream4ever <cnhwvista@gmail.com>
2024-01-10 15:54:14 +0800

    refactor: 修改渐变按钮组件的属性名称

--- packages/nextjs/components/GradientBorderButton.tsx
+++ packages/nextjs/components/GradientBorderButton.tsx
@@ -1,14 +1,14 @@
 type GradientBorderButtonProps = {
   btnText: string;
   className?: string;
-  onButtonClick?: () => void;
+  onClick?: () => void;
 };
 
-export const GradientBorderButton = ({ btnText, onButtonClick }: GradientBorderButtonProps) => {
+export const GradientBorderButton = ({ btnText, onClick }: GradientBorderButtonProps) => {
   return (
     <div
       className="relative flex items-center justify-center h-10 overflow-hidden font-semibold rounded-full"
-      onClick={onButtonClick}
+      onClick={onClick}
     >
       <div className="absolute top-0 bottom-0 left-0 right-0 bg-gradient-to-r from-gradFrom to-gradTo"></div>
       <div className="box-border z-10 flex items-center justify-center h-full px-8 border-2 border-transparent rounded-full bg-clip-padding bg-light">
