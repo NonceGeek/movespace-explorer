7639d5d94b2c05490047b2ce035651d0f8090a38
Dream4ever <cnhwvista@gmail.com>
2024-01-10 11:26:22 +0800

    feat: Panel 页面引入下拉菜单组件

--- packages/nextjs/pages/panel.tsx
+++ packages/nextjs/pages/panel.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import Image from "next/image";
 import type { NextPage } from "next";
+import { Select } from "~~/components/Select";
 import { SvgBNB } from "~~/components/svg/BNB";
 
 const fakeData = [
@@ -14,34 +15,60 @@ const fakeData = [
     tags: ["test", "test"],
   },
 ];
+const selectOptions = ["galxe-campaigns", "bodhi-text-contents"];
 
 const Panel: NextPage = () => {
   const [mockData, setMockData] = useState<any[]>([]);
+  const [showSelect, setShowSelect] = useState(false);
+  const [selectedOption, setSelectedOption] = useState<number | null>(null);
+  const [btnText, setBtnText] = useState("Select a DataSet");
 
   useEffect(() => {
     setMockData(fakeData);
     console.log(mockData);
   }, []);
 
+  const onSelectOptionClick = (index: number) => {
+    setSelectedOption(index);
+    setBtnText(selectOptions[index]);
+  };
+
   return (
     <div className="flex flex-col mx-auto w-content font-poppins">
-      <div className="flex items-center justify-between banner">
-        <div className="flex flex-col w-3/5 space-y-4 left">
-          <div className="flex flex-col space-y-9 upper">
-            <div className="flex flex-col space-y-6 intro">
+      {/* Banner */}
+      <div className="flex items-center justify-between">
+        {/* Left */}
+        <div className="flex flex-col w-3/5 space-y-4">
+          {/* Upper */}
+          <div className="flex flex-col space-y-9">
+            {/* Intro Text */}
+            <div className="flex flex-col space-y-6">
               <div className="text-6xl font-medium uppercase">Label data for AI training and earn rewards!</div>
               <div className="text-4xl">MoveSpace AI data labeler platform</div>
             </div>
             <div className="w-full h-px shrink-0 bg-purple-to-blue"></div>
           </div>
+          {/* Lower */}
           <div className="flex items-center space-x-3 lower">
             <SvgBNB className="text-yellow-400" />
             <div className="text-xl text-light-gray">Powered by BNB Greenfield , BNBChain & opBNB</div>
           </div>
         </div>
-        <div className="right">
+        {/* Right */}
+        <div className="">
           <Image src="/assets/artwork.png" width={600} height={574} alt="artwork" />
         </div>
+      </div>
+      {/* Action Buttons */}
+      <div className="flex items-center justify-center">
+        <Select
+          showSelect={showSelect}
+          options={selectOptions}
+          selectedOption={selectedOption}
+          onSelectClick={() => setShowSelect(!showSelect)}
+          btnText={btnText}
+          onSelectOptionClick={onSelectOptionClick}
+        />
       </div>
     </div>
   );
