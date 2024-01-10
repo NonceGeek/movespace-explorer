05c174beb8c91033db4cb781228b522e10059770
Dream4ever <cnhwvista@gmail.com>
2024-01-10 09:53:44 +0800

    chore: Footer 不再统一引入，而是只在需要的页面引入

--- packages/nextjs/pages/_app.tsx
+++ packages/nextjs/pages/_app.tsx
@@ -8,7 +8,6 @@ import NextNProgress from "nextjs-progressbar";
 import { Toaster } from "react-hot-toast";
 import { useDarkMode } from "usehooks-ts";
 import { WagmiConfig } from "wagmi";
-import { Footer } from "~~/components/Footer";
 import { Header } from "~~/components/Header";
 import { BlockieAvatar } from "~~/components/scaffold-eth";
 import { useNativeCurrencyPrice } from "~~/hooks/scaffold-eth";
@@ -66,7 +65,6 @@ const ScaffoldEthApp = ({ Component, pageProps }: AppProps) => {
             <main className="relative flex flex-col flex-1">
               <Component {...pageProps} />
             </main>
-            <Footer />
           </div>
         </ThemeProvider>
         <Toaster />
