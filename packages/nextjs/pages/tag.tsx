ee836156d69f6db0171b8be653d905ddc642e64e
Dream4ever <cnhwvista@gmail.com>
2024-01-10 09:24:47 +0800

    refactor: 删除用不到的代码

--- packages/nextjs/pages/tag.tsx
+++ packages/nextjs/pages/tag.tsx
@@ -2,9 +2,7 @@ import { useEffect, useState } from "react";
 import type { NextPage } from "next";
 import { useLocalStorage } from "usehooks-ts";
 import { MetaHeader } from "~~/components/MetaHeader";
-import { ContractUI } from "~~/components/scaffold-eth";
 import { ContractUIForBodhiTagger } from "~~/components/scaffold-eth";
-import { useScaffoldContractRead, useScaffoldContractWrite } from "~~/hooks/scaffold-eth";
 import { ContractName } from "~~/utils/scaffold-eth/contract";
 import { getContractNames } from "~~/utils/scaffold-eth/contractNames";
 
@@ -27,7 +25,8 @@ const Tag: NextPage = () => {
     const contractN = queryParameters.get("contract_name");
     setItemId(itemId);
     setContractName(contractN);
-  });
+  }, []);
+
   useEffect(() => {
     if (!contractNames.includes(selectedContract)) {
       setSelectedContract(contractNames[0]);
@@ -37,13 +36,13 @@ const Tag: NextPage = () => {
   return (
     <>
       <MetaHeader title="Tag Item | MoveSpace" description="Tag MoveSpace Items in Easy Way" />
-      <div className="flex flex-col gap-y-6 lg:gap-y-8 py-8 lg:py-12 justify-center items-center">
+      <div className="flex flex-col items-center justify-center py-8 gap-y-6 lg:gap-y-8 lg:py-12">
         {contractNames.length === 0 ? (
           <p className="text-3xl mt-14">No contracts found!</p>
         ) : (
           <>
             {contractNames.length > 1 && (
-              <div className="flex flex-row gap-2 w-full max-w-7xl pb-1 px-6 lg:px-10 flex-wrap">
+              <div className="flex flex-row flex-wrap w-full gap-2 px-6 pb-1 max-w-7xl lg:px-10">
                 {contractNames.map(contractName => (
                   <button
                     className={`btn btn-secondary btn-sm normal-case font-thin ${
