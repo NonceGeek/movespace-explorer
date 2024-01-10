00ee6d81a9ca02701b19c8988f9aba4a0d864b6e
Dream4ever <cnhwvista@gmail.com>
2024-01-10 09:11:58 +0800

    chore: 重命名组件

--- packages/nextjs/components/svg/Selected.tsx
+++ packages/nextjs/components/svg/Selected.tsx
@@ -3,7 +3,7 @@ export const SvgSelected = props => {
     <svg width={10} height={10} viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
       <path
         d="M8.558 2.251a.286.286 0 00-.197.087L3.986 6.713l-1.8-1.8a.286.286 0 10-.404.404L3.784 7.32a.286.286 0 00.404 0l4.577-4.576a.286.286 0 00-.207-.492z"
-        fill="#2F1073"
+        fill="currentColor"
       />
     </svg>
   );
