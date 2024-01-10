7ef44c1d49ff1b88a42e21891fae35f05915f80c
Dream4ever <cnhwvista@gmail.com>
2024-01-10 15:55:36 +0800

    chore: 渐变按钮组件悬浮时显示手型鼠标指针

--- packages/nextjs/components/GradientBorderButton.tsx
+++ packages/nextjs/components/GradientBorderButton.tsx
@@ -7,7 +7,7 @@ type GradientBorderButtonProps = {
 export const GradientBorderButton = ({ btnText, onClick }: GradientBorderButtonProps) => {
   return (
     <div
-      className="relative flex items-center justify-center h-10 overflow-hidden font-semibold rounded-full"
+      className="relative flex items-center justify-center h-10 overflow-hidden font-semibold rounded-full cursor-pointer"
       onClick={onClick}
     >
       <div className="absolute top-0 bottom-0 left-0 right-0 bg-gradient-to-r from-gradFrom to-gradTo"></div>
