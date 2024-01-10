6c575da75d9c363371b4ed5182ceff48f9b378dc
Dream4ever <cnhwvista@gmail.com>
2024-01-10 15:46:49 +0800

    feat: 完成 Footer2 组件

---
+++ packages/nextjs/components/Footer2.tsx
@@ -0,0 +1,11 @@
+export const Footer2 = () => {
+  return (
+    <div className="flex flex-col justify-between w-full pt-32 pb-16 space-y-3">
+      <div className="w-full h-px shrink-0 bg-purple-to-blue"></div>
+      <div className="flex items-center justify-between text-xl text-light-gray font-montserrat">
+        <span>Copyright © 2023 MOVESPACE. All rights reserved.</span>
+        <span>Terms of Use & Privacy Policy</span>
+      </div>
+    </div>
+  );
+};
