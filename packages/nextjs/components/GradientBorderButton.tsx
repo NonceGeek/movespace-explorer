0b8d1456a0469f61c234c3052953e0eda131bc70
Dream4ever <cnhwvista@gmail.com>
2024-01-10 16:25:22 +0800

    feat: 渐变按钮增加禁用状态

--- packages/nextjs/components/GradientBorderButton.tsx
+++ packages/nextjs/components/GradientBorderButton.tsx
@@ -1,17 +1,34 @@
 type GradientBorderButtonProps = {
   btnText: string;
+  disabled?: boolean;
   className?: string;
   onClick?: () => void;
 };
 
-export const GradientBorderButton = ({ btnText, onClick }: GradientBorderButtonProps) => {
+export const GradientBorderButton = ({ btnText, disabled = false, onClick }: GradientBorderButtonProps) => {
+  const onBtnClick = () => {
+    if (!disabled) {
+      !!onClick && onClick();
+    }
+  };
+
   return (
     <div
-      className="relative flex items-center justify-center h-10 overflow-hidden font-semibold rounded-full cursor-pointer"
-      onClick={onClick}
+      className={`relative flex items-center justify-center h-10 overflow-hidden font-semibold rounded-full ${
+        !disabled && "cursor-pointer"
+      }`}
+      onClick={onBtnClick}
     >
-      <div className="absolute top-0 bottom-0 left-0 right-0 bg-gradient-to-r from-gradFrom to-gradTo"></div>
-      <div className="box-border z-10 flex items-center justify-center h-full px-8 border-2 border-transparent rounded-full bg-clip-padding bg-light">
+      <div
+        className={`absolute top-0 bottom-0 left-0 right-0 ${
+          disabled ? "bg-gray-300" : "bg-gradient-to-r from-gradFrom to-gradTo"
+        }`}
+      ></div>
+      <div
+        className={`box-border z-10 flex items-center justify-center h-full px-8 border-2 border-transparent rounded-full bg-clip-padding bg-light ${
+          disabled && "text-gray-300"
+        }`}
+      >
         {btnText}
       </div>
     </div>
